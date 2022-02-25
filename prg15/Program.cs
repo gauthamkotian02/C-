80fd232a029795ebbae4354514c56c85b42fad82
gauthamkotian02 <98144065+gauthamkotian02@users.noreply.github.com>
2022-02-25 12:14:53 +0530

    Add files via upload

---
+++ prg15/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace prg15
+{
+    class ExceptionalHandling
+    {
+        static void Main(string[] args)
+        {
+            Age a = new Age();
+            try
+            {
+                a.displayAge();
+
+            }
+            catch (AgelsNegativeException e)
+            {
+                Console.WriteLine("AngelsNegativeException:{0}", e.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Execution of Finally block is done. ");
+
+            }
+        }
+    }
+}
+public class AgelsNegativeException : Exception
+{
+    public AgelsNegativeException(string message):base(message)
+    {
+
+    }
+}
+public class Age
+{
+    int age = -5;
+    public void displayAge()
+    {
+        if(age<0)
+        {
+            throw (new AgelsNegativeException("Age cannot be negative"));
+        }
+        else
+        {
+            Console.WriteLine("Age is:{0}", age);
+        }
+    }
+}
